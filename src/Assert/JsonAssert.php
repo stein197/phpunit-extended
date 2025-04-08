c2a96cce68461c0394a29d2612e7ea82bc7a89e8
Nail Gafarov <nail.gafarov@bikar.com>
2025-04-08 14:58:18 +0200

    feat(json): :sparkles: add `JsonAssert->assertCount()` method

--- src/Assert/JsonAssert.php
+++ src/Assert/JsonAssert.php
@@ -3,6 +3,7 @@ namespace Stein197\PHPUnit\Assert;
 
 use JsonPath\JsonObject;
 use PHPUnit\Framework\TestCase;
+use function sizeof;
 
 // TODO: response()->json()
 /**
@@ -20,4 +21,19 @@ final readonly class JsonAssert {
 		private TestCase $test,
 		private JsonObject $json
 	) {}
+
+	/**
+	 * Assert that there are `$expectedCount` elements matching the `$query`.
+	 * @param string $query JSONPath to find elements by.
+	 * @param int $expectedCount Expected amount of elements to find.
+	 * @return void
+	 * @throws ExpectationFailedException If the amount of the found elements is not equal to the `$expectedCount`.
+	 * ```php
+	 * $this->assertCount('$.user', 10);
+	 * ```
+	 */
+	public function assertCount(string $query, int $expectedCount): void {
+		$length = sizeof($this->json->get($query));
+		$this->test->assertEquals($expectedCount, $length, "Expected to find {$expectedCount} elements matching the JSONPath \"{$query}\", actual: {$length}");
+	}
 }
--- test/Assert/JsonAssertTest.php
+++ test/Assert/JsonAssertTest.php
@@ -1,11 +1,40 @@
 <?php
 namespace Test\Assert;
 
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use Stein197\PHPUnit\Assert\JsonAssert;
 use Stein197\PHPUnit\TestCase;
 
-// TODO
 final class JsonAssertTest extends PHPUnitTestCase {
 
 	use TestCase;
+
+	#[Test]
+	#[DataProvider('dataAssertCount')]
+	#[TestDox('assertCount()')]
+	public function testAssertCount(?string $exceptionMessage, string $json, string $query, int $expectedCount): void {
+		$this->assert($exceptionMessage, $json, static function (JsonAssert $assert) use ($query, $expectedCount): void {
+			$assert->assertCount($query, $expectedCount);
+		});
+	}
+
+	public static function dataAssertCount(): array {
+		return [
+			'passed' => [null, '{"user": [{}, {}]}', '$.user[*]', 2],
+			'failed' => ['Expected to find 2 elements matching the JSONPath "$.user[*]", actual: 0', '{"user": []}', '$.user[*]', 2],
+			'failed and JSONPath not exists' => ['Expected to find 2 elements matching the JSONPath "$.user[*]", actual: 0', '{}', '$.user[*]', 2],
+		];
+	}
+
+	private function assert(?string $exceptionMessage, string $json, callable $f): void {
+		if ($exceptionMessage) {
+			$this->expectException(AssertionFailedError::class);
+			$this->expectExceptionMessage($exceptionMessage);
+		}
+		$f($this->json($json));
+	}
 }
